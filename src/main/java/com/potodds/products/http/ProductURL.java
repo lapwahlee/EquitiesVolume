2f6e69762e124d4f74dff513d46ac37666403a4a
lap <lap@CUSTOMPC_2010>
2017-08-05 14:45:15 -0400

    Finviz auth string

--- src/main/java/com/potodds/products/http/ProductURL.java
+++ src/main/java/com/potodds/products/http/ProductURL.java
@@ -7,7 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ProductURL {
-	private final String finvizURL = "http://elite.finviz.com/export.ashx?v=151&f=ind_stocksonly";	
+	private final String finvizURL = "http://elite.finviz.com/export.ashx?auth=lapwah.lee@gmail.com&v=151&f=ind_stocksonly";	
 	
 	public void run() {
 
@@ -16,10 +16,9 @@ public class ProductURL {
 			obj = new URL(finvizURL);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 			
-            String userpass = "lapwah.lee@gmail.com" + ":" + "murano2005";
-            String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes());
-
-            con.setRequestProperty ("Authorization", basicAuth);
+//            String userpass = "lapwah.lee@gmail.com" + ":" + "murano2005";
+//            String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes());
+//            con.setRequestProperty ("Authorization", basicAuth);
 
 			// optional default is GET
 //			con.setRequestMethod("GET");
